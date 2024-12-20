fc127f178b3e6887570eb7822eb1e26dbed2f4dd
Nitin Garg <ngarg8487@gmail.com>
2024-12-20 12:46:00 +0530

    Updated Endpoints and Errors Handled

--- main.py
+++ main.py
@@ -46,31 +46,46 @@ def get_MCX():
 def home():
     return {"Message": "API is Up and Running!"}
 
-@app.get("/get_rate")
-def get_rate():
-    gold, silver = get_MCX()
-    return {"goldMCX":gold, "silverMCX":silver}
+# @app.get("/get_rate")
+# def get_rate():
+#     gold, silver = get_MCX()
+#     return {"goldMCX":gold, "silverMCX":silver}
 
 @app.post("/set_differences")
 def create_price(price: RateModel):
-    price_dict = price.model_dump()
-    price_dict["date"] = date.today().isoformat()
-    already = app.collection.find_one({"date": date.today().isoformat()})
-    if already:
-        app.collection.update_one({"date": date.today().isoformat()}, {"$set":price_dict})
-        return {"Message": "Updated"}
-    else:
-        result = app.collection.insert_one(price_dict)
-        return {"Message": "Inserted"}
-        
+    try:
+        price_dict = price.model_dump()
+        price_dict["date"] = date.today().isoformat()
+        already = app.collection.find_one({"date": date.today().isoformat()})
+        if already:
+            app.collection.update_one({"date": date.today().isoformat()}, {"$set":price_dict})
+            return {"Message": "Updated"}
+        else:
+            result = app.collection.insert_one(price_dict)
+            return {"Message": "Inserted"}
+    except Exception as e:
+        print(e)
+        return HTTPException(400, "Some Error Occured! Check Logs")
 
-@app.get("/get_differences")
-def get_price():
-    entry = app.collection.find_one({"date": date.today().isoformat()})
-    if(entry):
-        return {"gold_cash": entry["gold_cash"],"silver_cash": entry["silver_cash"],"gold_rtgs": entry["gold_rtgs"],"silver_rtgs": entry["silver_rtgs"]}
-    else:
-        HTTPException(404, "Not Found Today's Entry")
+# @app.get("/get_differences")
+# def get_price():
+#     entry = app.collection.find_one({"date": date.today().isoformat()})
+#     if(entry):
+#         return {"gold_cash": entry["gold_cash"],"silver_cash": entry["silver_cash"],"gold_rtgs": entry["gold_rtgs"],"silver_rtgs": entry["silver_rtgs"]}
+#     else:
+#         HTTPException(404, "Not Found Today's Entry")
 
-
+@app.get("/get_data")
+def get_data():
+    try:
+        gold, silver = get_MCX()
+        entry = app.collection.find_one(sort=[('$natural', -1)])
+        if(entry):
+            return {"gold_cash": entry["gold_cash"],"silver_cash": entry["silver_cash"],"gold_rtgs": entry["gold_rtgs"],"silver_rtgs": entry["silver_rtgs"],
+                "gold_mcx":gold, "silver_mcx":silver}
+        else:
+            return HTTPException(400, "Not Found Any Entry")
+    except Exception as e:
+        print(e)
+        return HTTPException(400, "Some Error Occured! Check Logs")
 
